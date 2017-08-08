177baf4a40c4aa9f0830910c40aa1d56799b7ffa
Vinod <vtkrishn@gmail.com>
2017-08-07 19:55:43 -0700

    remove app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var redis = require("redis");
-var client = redis.createClient();
-client.on('connect', function() {
-    console.log('connected');
-});
-
-client.get('vinod', function(err, reply) {
-    console.log(reply);
-});
